7cdb998c3fc807c7e9149b7805fb67760db77f48
ericknina20 <ericknina20@gmail.com>
2016-05-15 10:00:23 -0500

    Primer parche variables y analizador

---
+++ analitza.h
@@ -0,0 +1,20 @@
+#ifndef ANALITZA
+#define ANALITZA
+
+#include <iostream>
+#include <cmath>
+#include <qstring.h>
+#include <qlocale.h>
+#include <qdom.h>
+#include <qfile.h>
+#include <qxml.h>
+#include <qvaluelist.h>
+#include <qtextstream.h>
+#include <qmessagebox.h>
+#include "qexp.h"
+#include "variables.h"
+
+using namespace std;
+
+#endif
+
