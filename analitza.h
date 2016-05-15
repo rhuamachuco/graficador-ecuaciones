d62659a45714ce59f658e55be955fe6c6a21b83c
ericknina20 <ericknina20@gmail.com>
2016-05-15 13:40:05 -0500

    actualización header analizador

--- analitza.h
+++ analitza.h
@@ -16,6 +16,37 @@
 
 using namespace std;
 
+void print_dom(QDomNode, int ind=0);
+
+class Analitza{
+public:
+
+    Analitza(QString path);
+    Analitza();
+    ~Analitza();
+
+    QDomNode elem;
+    static int isOperador(QString);
+    int setPath(QString path);
+    int setTextMML(QString exp);
+    int setText(QString exp);
+    QString textMML();
+    bool simplify();
+
+    void setVars(Variables v);
+    double Calcula();
+    QStringList lambda(); //retrieve lambda vars
+    double toNum(QDomElement res);
+
+    static bool isNum(QString);
+    static QString treu_tags(QString in);
+    static QDomElement toCn(double);
+    static bool takesQualifiers(QString);
+    static QStringList bvar(QDomNode);
+    static bool isMathML(QString exp);
+
+    Variables vars;
+    QString err;
 
 
 #endif
