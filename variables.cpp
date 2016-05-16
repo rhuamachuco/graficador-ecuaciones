d8c88e335270e57b83226c4cd38b86f6eb26a95e
rhuamachuco <romelhuamachuco@gmail.com>
2016-05-16 10:33:45 -0500

    find

--- variables.cpp
+++ variables.cpp
@@ -63,3 +63,74 @@ bool Variables::modifica(QString id, QDomElement new_val)
 	}
 	return ex;
 }
+
+QDomElement* Variables::find(QString id)
+{
+	QDomElement* ret=NULL;
+	bool ex=false;
+		
+	QValueList<struct VARIABLE>::iterator it;
+	for(it = vars.begin(); !ex && it != vars.end(); ++it){ 
+		if((*it).nom == id){
+			ret=&(*it).valor;
+			ex=true;
+		}
+	}
+	return ret;
+}
+
+QDomElement Variables::value(QString id, bool *ex){
+	QValueList<struct VARIABLE>::iterator it;
+	
+	for(it = vars.begin(); it != vars.end(); ++it){
+		if((*it).nom.stripWhiteSpace() == id.stripWhiteSpace()){
+			if(ex!=NULL) *ex=false;
+			return (*it).valor;
+		}
+	}
+	//error
+	QDomDocument a; QString err("<cn \"constant\"> &NaN; </cn>");
+	a.setContent(err);
+	if(ex!=NULL) *ex=true;
+	return a.documentElement();
+}
+
+QStringList Variables::getNoms(){
+	struct VARIABLE aux;
+	QStringList out;
+	QValueList<struct VARIABLE>::iterator it;
+	for(it = vars.begin(); it != vars.end(); ++it){ 
+		aux = *it;
+		out << aux.nom;
+	}
+	return out;
+}
+
+bool Variables::isvar(QDomElement e){
+	if(e.tagName()=="ci"){
+// 		struct VARIABLE aux;
+		QStringList out;
+		QValueList<struct VARIABLE>::iterator it;
+		for(it = vars.begin(); it != vars.end(); ++it){ 
+			if((*it).nom==e.text())
+				return true;
+		}
+	}
+	return false;
+}
+
+bool Variables::rename(QString id, QString newId){
+	bool ex=false;
+	struct VARIABLE aux;
+	QValueList<struct VARIABLE>::iterator it;
+	
+	for(it = vars.begin(); !ex && it != vars.end(); ++it){ 
+		aux = *it;
+		if(aux.nom == id.stripWhiteSpace()){
+			ex=true;
+			(*it).nom=newId;
+		}
+	}
+	return ex;
+}
+
